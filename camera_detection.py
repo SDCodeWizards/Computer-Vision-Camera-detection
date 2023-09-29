e4cda646ccda6398e9880a08a127ea97a256decd
yinshinLi <93513827+yinshinLi@users.noreply.github.com>
2023-09-28 21:26:49 -0700

    init hand detection library

--- camera_detection.py
+++ camera_detection.py
@@ -1,4 +1,10 @@
 import cv2 as cv
+import numpy as np
+import mediapipe as mp
+
+# Initialize MediaPipe Hands model
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands()
 
 try:
     c = cv.VideoCapture(0) # Capture the default camera
@@ -8,6 +14,21 @@ except:
 while True:
     # Capture each frame within the camera
     ret, frame = c.read()
+
+
+    # Convert the frame to RGB for hand detection
+    rgb_frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+
+    # Detect hands in the frame
+    results = hands.process(rgb_frame)
+
+    if results.multi_hand_landmarks:
+        # Draw landmarks on the frame
+        for landmarks in results.multi_hand_landmarks:
+            for point in landmarks.landmark:
+                x, y = int(point.x * frame.shape[1]), int(point.y * frame.shape[0])
+                cv.circle(frame, (x, y), 5, (0, 255, 0), -1)
+
     # Display the frame with imshow.
     cv.imshow("Camera Feed", frame)
 
@@ -18,3 +39,4 @@ while True:
 # Release camera and close all opencv windows.
 c.release()
 cv.destroyAllWindows()
+hands.close()
