12b0f76dac0038294738b6868d9d811e6a892e90
Guan Li <gcl001@ucsd.edu>
2023-09-28 17:23:59 -0700

    initialize testing

--- camera_detection.py
+++ camera_detection.py
@@ -1,13 +1,5 @@
 import cv2 as cv
 
-def testDevice(source):
-   cap = cv.VideoCapture(source) 
-   if cap is None or not cap.isOpened():
-       print('Warning: unable to open video source: ', source)
-
-# testDevice(0) # no printout
-# testDevice(1) # prints message
-
 try:
     c = cv.VideoCapture(0) # Capture the default camera
 except:
