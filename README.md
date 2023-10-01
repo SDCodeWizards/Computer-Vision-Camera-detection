5291788781e4c14832b875a0203797d79f4995bc
xiaoxia-xia <xiaoxiaxia88@gmail.com>
2023-09-30 22:37:01 -0700

    orphan

---
+++ README.md
@@ -0,0 +1,26 @@
+# Computer-Vision #
+
+## How to install open-cv2 library ##
+```
+pip install opencv-python
+```
+
+Then you can import libary with 
+```
+import cv2 as cv
+```
+ 
+## If you are working with Visual studio and wonders how to create your IntelliSense for cv2 here is how ##
+use the find_cv2_path.py file to find your installed cv2 location, Then create a folder with the name .vscode and create a file within the folder called settings.json
+This file will adjust your visual studio code's settings.
+
+Write the settings below:
+```
+{
+    "python.autoComplete.addBrackets": true,
+    "python.analysis.autoSearchPaths": true,
+    "python.analysis.extraPaths": ["path/to/your/opencv/python/library"]
+}
+
+```
+make sure to insert the path that you find in the cv2 location python file into the python.analysis.extraPaths parameter replacing the string.
---
+++ camera_detection.py
@@ -0,0 +1,53 @@
+import cv2 as cv
+import numpy as np
+import mediapipe as mp
+from utils import recognizer
+
+
+
+# Initialize MediaPipe Hands model
+recognizer = recognizer.recognizer()
+
+try:
+    c = cv.VideoCapture(0) # Capture the default camera
+except:
+    print("Default Camera is currently invalid")
+
+
+while True:
+    # Capture each frame within the camera
+    ret, frame = c.read()
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+
+    # Convert the frame to RGB for hand detection
+    rgb_frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+    mp_rgb_frame = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb_frame)
+    # The detector is initialized. Use it here.
+    gesture_recognition_result = recognizer.recognize(mp_rgb_frame)
+    if gesture_recognition_result.gestures and gesture_recognition_result.gestures[0]:
+        
+        posture = gesture_recognition_result.gestures[0][0].category_name
+        cv.putText(frame, posture, (100,100), cv.FONT_HERSHEY_SIMPLEX, 1,(255, 0, 0), 3, cv.LINE_AA)
+
+        handness = gesture_recognition_result.handedness[0][0].category_name
+        cv.putText(frame, handness, (300,100), cv.FONT_HERSHEY_SIMPLEX, 1,(255, 0, 255), 3, cv.LINE_AA)
+
+        # Draw landmarks on the frame(green dots) 
+        for landmarks in gesture_recognition_result.hand_landmarks[0]:
+            x, y = int(landmarks.x * frame.shape[1]), int(landmarks.y * frame.shape[0])
+            cv.circle(frame, (x, y), 5, (0, 255, 0), -1)
+
+    # Display the frame with imshow.
+    cv.imshow("Camera Feed", frame)
+
+
+
+# Stop the images showing after q pressed:
+    if cv.waitKey(1) & 0xFF == ord("q"):
+        break
+    
+# Release camera and close all opencv windows.
+c.release()
+cv.destroyAllWindows()
