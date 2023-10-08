79532bd61f6112fe7c3102e07f442d21f694f5c7
Guan Li <gcl001@ucsd.edu>
2023-10-08 15:28:12 -0700

    frame settings completed
    - set frame in variable.py
    - optimized and reduced calculations required.
    - can now delay detection on images.

---
+++ utils/variables.py
@@ -0,0 +1,7 @@
+# Variables
+frame_counter = 0 # For how many frame per calculations.
+box_counter = 0 # Counts how many boxes are drawn on screen
+box_location = [] # Stores as (initial_x, initial_y, final_x, final_y, name_of_prediction)
+total_box_amounts = 1 # Amount on how many boxes to display on screen.
+frame_setter = 1 # Set how many frames
+cycle = False
